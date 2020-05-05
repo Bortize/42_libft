601d54062dccfbea951344b82e6d2845659c8cbf
Borja <55995077+Bortize@users.noreply.github.com>
2020-05-05 11:52:32 +0200

    Update ft_strjoin.c

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -10,6 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+** Asigna memoria para devolver una cadeda terminada en '\0', que sera el resultado 
+** de la concatenacion de la cadena s1 y s2. Si la asignacion falla devuelve NULL
+*/
+
 #include "libft.h"
 
 char		*ft_strjoin(char const *s1, char const *s2)
@@ -18,14 +23,14 @@ char		*ft_strjoin(char const *s1, char const *s2)
 	size_t	str1;
 	size_t	str2;
 
-	if (s1 == 0 || s2 == 0)
+	if (s1 == 0 || s2 == 0) // Si cualquiera de las 2 cadenas no existe, devuelve 0
 		return (0);
-	str1 = ft_strlen(s1);
-	str2 = ft_strlen(s2);
-	str = (char *)malloc(sizeof(char) * (str1 + str2 + 1));
-	if (str == 0)
+	str1 = ft_strlen(s1); // asigna la longitud de s1
+	str2 = ft_strlen(s2); 
+	str = (char *)malloc(sizeof(char) * (str1 + str2 + 1)); // Reserva memoria para la nueva cadena reservando 1 para el NULL del final
+	if (str == 0) // Si falla la reserva de memoria devuelve 0
 		return (0);
-	ft_strcpy(str, s1);
-	ft_strlcat(str, s2, (str1 + str2 + 1));
+	ft_strcpy(str, s1); // Copia la cadena s1 en 'str'
+	ft_strlcat(str, s2, (str1 + str2 + 1)); // Devuelve la longitud de bytes de la suma de la concatenacion de dst y src
 	return (str);
 }
