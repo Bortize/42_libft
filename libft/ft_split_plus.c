fd5632e3a785fd10c277e590c40ae0232e528035
Borja GRD <bgomez-r@student.42madrid.com>
2020-12-12 18:36:25 +0100

    add the ft_split_plus function to the library

---
+++ libft/ft_split_plus.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split_plus.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bortize <bortize@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/12 18:28:45 by bortize           #+#    #+#             */
+/*   Updated: 2020/12/12 18:29:22 by bortize          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int			ft_countword(char const *s, char c, char t)
+{
+	unsigned int	i;
+	int				words;
+
+	i = 0;
+	words = 0;
+	while (s[i])
+	{
+		while (s[i] == c || s[i] == t)
+			i++;
+		if (s[i] != '\0')
+			words++;
+		while (s[i] && ((s[i] != c) && (s[i] != t)))
+			i++;
+	}
+	return (words);
+}
+
+
+char		*ft_strncpy(char *dst, const char *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] && i < n)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dst[i] = '\0';
+		i++;
+	}
+	return (dst);
+}
+
+
+static char			*ft_wordcopy(const char *s, size_t n)
+{
+	char	*str;
+
+	str = (char *)malloc(sizeof(char) * n + 1);
+	if (!str)
+		return (NULL);
+	str = ft_strncpy(str, s, n);
+	str[n] = '\0';
+	return (str);
+}
+
+
+char				**ft_split_plus(char const *s, char c, char t)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	**tab;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	k = 0;
+	if (!(tab = (char **)malloc(sizeof(char *) * (ft_countword(s, c, t)) + 1)))
+		return (NULL);
+	while (s[i])
+	{
+		while (s[i] == c || s[i] == t)
+			i++;
+		j = i;
+		while (s[i] && ((s[i] != c) && (s[i] != t)))
+			i++;
+		if (i > j)
+		{
+			tab[k++] = ft_wordcopy(s + j, i - j);
+		}
+	}
+	tab[k] = NULL;
+	return (tab);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bgomez-r <bgomez-r@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bortize <bortize@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 12:53:15 by bgomez-r          #+#    #+#             */
-/*   Updated: 2020/01/04 19:00:32 by bgomez-r         ###   ########.fr       */
+/*   Updated: 2020/12/12 18:30:18 by bortize          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,5 +70,6 @@ void			ft_lstclear(t_list **lst, void (*del)(void *));
 void			ft_lstiter(t_list *lst, void (*f)(void *));
 t_list			*ft_lstmap(t_list *lst, void *(*f)(void *),
 				void (*del)(void *));
+char			**ft_split_plus(char const *s, char c, char t);
 
 #endif
