0af52456fa3e0d159286e6919c3a108a42882222
Borja <bgomez-r@student.42madrid.com>
2021-03-25 21:37:36 +0100

    Add a function

--- libft/libft.h
+++ libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bortize <bortize@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bgomez-r <bgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 12:53:15 by bgomez-r          #+#    #+#             */
-/*   Updated: 2020/12/12 18:49:54 by bortize          ###   ########.fr       */
+/*   Updated: 2021/03/25 21:37:06 by bgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,5 +72,6 @@ t_list			*ft_lstmap(t_list *lst, void *(*f)(void *),
 				void (*del)(void *));
 char			**ft_split_plus(char const *s, char c, char t);
 char			*ft_strncpy(char *dst, const char *src, size_t n);
+int				ft_countchar(char *str, char c);
 
 #endif
